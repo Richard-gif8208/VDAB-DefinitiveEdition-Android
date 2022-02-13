8e9c9767a5d023206a7e2ac93d2e05b7c1b2cdaa
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-13 18:38:01 +0200

    Update DialogueBox.hx

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -308,7 +308,21 @@ class DialogueBox extends FlxSpriteGroup
 			dialogueStarted = true;
 		}
 
-		if (FlxG.keys.justPressed.ANY  && dialogueStarted)
+
+		    #if android
+		    var justTouched:Bool = false;
+
+		    for (touch in FlxG.touches.list)
+		    {
+			    justTouched = false;
+
+			    if (touch.justPressed){
+				    justTouched = true;
+			    }
+		    }
+		    #end			
+
+		if (FlxG.keys.justPressed.ANY#if android || justTouched #end  && dialogueStarted)
 		{
 			remove(dialogue);
 			
